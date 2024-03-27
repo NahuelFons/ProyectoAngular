f2ab1944ea4b2cdd2bacd0295ec1223688f1c07f
Nahuel Fons <nahuelfons@gmail.com>
2024-03-26 22:10:00 -0300

    UPDATE: Lista de alumnos + Directivas

--- src/app/content-page/content-page.component.ts
+++ src/app/content-page/content-page.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class ContentPageComponent {
 
+  entregado = true;
+  aprobado = true;
+  alternarCalificacion(student: any) {
+    student.aprobado = !student.aprobado;
+}
+  
+  students = [
+    {nombre: 'Alejandro', apellido: 'Garcia', entregado: true, aprobado: true},
+    {nombre: 'Sofia', apellido: 'Rodriguez', entregado: true, aprobado: false},
+    {nombre: 'Diego', apellido: 'Martinez', entregado: false, aprobado: false},
+    {nombre: 'Valentina', apellido: 'Lopez', entregado: false, aprobado: false},
+    {nombre: 'Santiago', apellido: 'Hernandez', entregado: true, aprobado: false}
+  ];
+
+  entregaLimite = new Date (2024, 3, 27);
 }
